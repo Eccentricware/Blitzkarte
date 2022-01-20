c8018f3412c48df392c3fe6a85471c08241e738d
Joseph Peck <joe.r.peck@gmail.com>
2022-01-19 22:52:54 -0800

    Properly parses labels and nodes

---
+++ blitzkarte/utils/Parsing/pin.ts
@@ -0,0 +1,54 @@
+export class Pin {
+  pinType: string | undefined;
+  nodeType: string | undefined;
+  name: string | undefined;
+  province: string | undefined;
+  labelType: string | undefined;
+  text: string | undefined;
+  loc: number[] | undefined;
+  adj: string[] | undefined;
+
+  constructor() {}
+
+  isValidNode(): boolean {
+    let acceptedKeys: string[] = [
+      'pinType',
+      'nodeType',
+      'name',
+      'province',
+      'labelType',
+      'text',
+      'loc',
+      'adj'
+    ];
+
+    for (let key in this) {
+      if (!acceptedKeys.includes(key)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  isValidLabel(): boolean {
+    let acceptedKeys: string[] = [
+      'pinType',
+      'nodeType',
+      'name',
+      'province',
+      'labelType',
+      'text',
+      'loc',
+      'adj'
+    ];
+
+    for (let key in this) {
+      if (!acceptedKeys.includes(key)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
