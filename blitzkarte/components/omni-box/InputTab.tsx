f1f99323c9985bef92c971dbd1a8126a7c7b7750
Joseph Peck <joe.r.peck@gmail.com>
2022-05-03 13:48:24 -0700

    Circle descriptor differentiating types is in play

--- blitzkarte/components/omni-box/InputTab.tsx
+++ blitzkarte/components/omni-box/InputTab.tsx
@@ -23,14 +23,33 @@ export const InputTab: FC<InputProps> = ({input}: InputProps) => {
         <p>
           For the map to parse the layer names in Illustrator must follow a certain format.Improper formatting or detected mismatching elements will provide &quot;warning&quot;, &quot;error&quot;, or &quot;criticial&quot; feedback. Many of the properties can be abbreviated as noted. ALL properties should be together without any spaces, separated by commas, with an equal sign to assign the value.<br />
           <br />
-          <b>Provinces:</b><br />
+
+          <b>SVG Element: g | Provinces</b><br />
           <b>Example Full:</b> name=SYD,fullName=Sydney,type=coast,country=Germany<br />
           <b>Example Abbreviated:</b> n=SYD,f=Sydney,t=c,c=Germany<br />
           <br />
-          <b>name (n)</b>: The all-caps abbreviation (SYD) displayed on the map. Required unless decorative province.<br />
-          <b>fullName (f)</b>: The real-world name of the location (Sydney). Never required. Use underscores if there are spaces. Ex: New_York_City<br />
-          <b>type (t)</b>: Type of province impacts game rules and helps with validation and design.Always required.Options: coast (c), decorative (d), impassible (im), inland (i), island (is), sea (s), land (l), pole (p)<br />
-          <b>country (c)</b>: Leave empty if no country owns it. Use underscores instead of spaces. Ex: United_States<br />
+          <b>name (n):</b> The all-caps abbreviation (SYD) displayed on the map. Required unless decorative province.
+          <br />
+          <b>fullName (f):</b> The real-world name of the location (Sydney). Never required. Use underscores if there are spaces. Ex: New_York_City
+          <br />
+          <b>type (t):</b> Type of province impacts game rules and helps with validation and design.Always required.Options: coast (c), decorative (d), impassible (im), inland (i), island (is), sea (s), land (l), pole (p)
+          <br />
+          <b>country (c):</b> Leave empty if no country owns it. Use underscores instead of spaces. Ex: United_States
+          <br />
+          <br/>
+
+          <b>SVG Element: circle | Nodes, Cities, Labels</b>
+          <br/>
+          <b>PinType (p):</b> Notes what logic is applied to the circle. Alway required. node (n) for nodes (where units can go, and if there is a unit there), city (c) to establish the presence and status of a supply center/vote, and label (l) for the location of all labels. Provinces can have more than 1 label on account of provinces that wrap around the map and multi-coast provinces.
+          <br/>
+          <br/>
+          <b>Nodes (Where units can go, location of invasion arrow, retreating unit, nuclear detonations.</b>
+          <br/>
+          <b>Example Full:</b> pinType=node,type=sea,name=syd_s,adj=bri_s/tas_s/mel_s
+          <br/>
+          <b>Example Abbreviated:</b> p=n,t=s,n=syd_s,a=bri_s/tas_s/mel_s
+          <br/>
+          <b>pinType (p):</b> Choose n or node. Other
         </p>
       </div>
     </div>
