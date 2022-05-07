f09a4558b104641c02c067c5612b89e3a66a469e
Joseph Peck <joe.r.peck@gmail.com>
2022-05-06 18:34:59 -0700

    You want to do some labels? Now you have guidelines

--- blitzkarte/components/omni-box/InputTab.tsx
+++ blitzkarte/components/omni-box/InputTab.tsx
@@ -43,8 +43,7 @@ export const InputTab: FC<InputProps> = ({input}: InputProps) => {
           <b>PinType (p):</b> Notes what logic is applied to the circle. Alway required. node (n) for nodes (where units can go, and if there is a unit there), city (c) to establish the presence and status of a supply center/vote, and label (l) for the location of all labels. Provinces can have more than 1 label on account of provinces that wrap around the map and multi-coast provinces.
           <br/>
           <br/>
-          <b>Nodes (Where units can go, location of invasion arrow, retreating unit, nuclear detonations)</b>
-          <br/>
+          <b>Nodes (Where units can go, location of invasion arrow, retreating unit, nuclear detonations)</b><br/>
           <b>pinType (p):</b> node (n)<br/>
           <b>Example Full:</b> pinType=node,type=sea,name=syd_s,adj=bri_s/tas_s/mel_s
           <br/>
@@ -55,11 +54,16 @@ export const InputTab: FC<InputProps> = ({input}: InputProps) => {
           <b>adj (a)</b>: Adjacent nodes. List of the names of nodes the unit type can travel to. Split with &quot; /&quot;. Required if not an event node. Example: a=bri_s/tas_s/mel_s<br/>
           <b>unit (u):</b> army (a), fleet (f), wing (w), nuke (n), garrison (g). Not required<br/>
           <br/>
-          <b>Cities (votes, capitals, supply centers)</b><br/>
+          <b>Cities (Designates capitals, supply and voting centers)</b><br/>
           <b>pinType (p):</b> city (c)<br/>
           <b>Example Full:</b> pinType=city,type=capital,country=Australia,rank=d,color=#6da6db<br/>
           <b>Example Abbreviated:</b> p=c,t=c,country=Australia,rank=d,color=#6da6db<br/>
-          <b>city (c):</b> capital (c), vote (v), supplyCenter (s) is used for controlled cities, dormant (d) used for uncontrolled cities. Do not add if province does not have a city. If it is a capital, furhter information is required. <b>Country:</b> Name of the country. Use _ instead of spaces. <b>rank:</b> a, b, c, d, e for players, n for non-player countries (where the garrisons are assigned). <b>color:</b> in valid CSS format. It should be easy to get the hex version (#ffffff) from Illustrator.
+          <b>city (c):</b> capital (c), vote (v), supplyCenter (s) is used for controlled cities, dormant (d) used for uncontrolled cities. Do not add if province does not have a city. If it is a capital, furhter information is required. <b>Country:</b> Name of the country. Use _ instead of spaces. <b>rank:</b> a, b, c, d, e for players, n for non-player countries (where the garrisons are assigned). <b>color:</b> in valid CSS format. It should be easy to get the hex version (#ffffff) from Illustrator.<br/>
+          <br/>
+          <b>Labels (Displays province name or specific coasts)</b><br/>
+          <b>pinType (p):</b> label (l). Always required.<br/>
+          <b>number (n):</b> Set to any number to distinguish labels apart. Useful for provinces that wrap around the map and multi-coast provinces. Only required when multiple lables are used in the same province.<br/>
+          <b>text:</b> Leave blank if the province label. It will auto-populate. Use this to name coasts (nc, wc, ec, sc, etc). Not required<br/>
         </p>
       </div>
     </div>
