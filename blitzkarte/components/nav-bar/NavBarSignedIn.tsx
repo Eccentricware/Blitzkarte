6598521467f38b76e7a416b62f290ba439423fd6
Joseph Peck <joe.r.peck@gmail.com>
2022-06-14 20:45:18 -0700

    Navbar section spacing

--- blitzkarte/components/nav-bar/NavBarSignedIn.tsx
+++ blitzkarte/components/nav-bar/NavBarSignedIn.tsx
@@ -1,5 +1,5 @@
 import React, { FC, Fragment, useState } from 'react';
-import { AppBar, Button, Menu, MenuItem, TextField, Toolbar, Typography } from '@mui/material';
+import { AppBar, Box, Button, Container, IconButton, Menu, MenuItem, Toolbar, Typography } from '@mui/material';
 import { useRouter } from 'next/router';
 import { getAuth, signOut } from 'firebase/auth';
 
@@ -19,6 +19,15 @@ export const NavBarSignedIn: FC<AppBarProps> = ({title}: AppBarProps) => {
   }
 
   const handleProfileMenuClose = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(null);
+  }
+
+  const handleGameSelectionMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+    console.log('url', router.pathname);
+  }
+
+  const handleGameSelectionMenuClose = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorEl(null);
   }
 
@@ -51,12 +60,70 @@ export const NavBarSignedIn: FC<AppBarProps> = ({title}: AppBarProps) => {
         <Button onClick={handleProfileMenuClose}>X</Button>
       </MenuItem>
     </Menu>
-  )
+  );
+
+  const gameSelectionMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'right'
+      }}
+      open={isMenuOpen}
+    >
+      {/* {router.pathname !== '/dashboard' &&
+        <MenuItem>
+          <Button onClick={handleDashboardClick}>Dashboard</Button>
+        </MenuItem>
+      } */}
+      <MenuItem>
+        {/* <Button onClick={handleSignOutClick}>Log Out</Button> */}
+        <Button onClick={handleGameSelectionMenuClose}>X</Button>
+      </MenuItem>
+    </Menu>
+  );
 
   return (
-    <Fragment>
-      <AppBar position="sticky">
+    <Box sx={{ flexGrow: 1}}>
+      <AppBar position="static">
         <Toolbar>
+          <Container>
+            <Typography align="left" variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Next Deadline
+            </Typography>
+          </Container>
+            {gameSelectionMenu}
+          <Container>
+
+          <Typography align="center" variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Blitzkarte
+          </Typography>
+          </Container>
+          <Container>
+            <Typography align="right" variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              <Button
+                disabled
+                color="inherit"
+              >
+                Alerts
+              </Button>
+              <Button
+                disabled
+                color="inherit"
+              >
+                Messages
+              </Button>
+              <Button
+                onClick={handleProfileMenuOpen}
+                color="inherit"
+              >
+                Person Icon
+              </Button>
+            </Typography>
+
+          </Container>
+        </Toolbar>
+        {/* <Toolbar>
           <Typography variant="h6"
             noWrap
             component="div"
@@ -87,9 +154,9 @@ export const NavBarSignedIn: FC<AppBarProps> = ({title}: AppBarProps) => {
           >
             Person Icon
           </Button>
-        </Toolbar>
+        </Toolbar> */}
       </AppBar>
       {profileMenu}
-    </Fragment>
+    </Box>
   )
 }
